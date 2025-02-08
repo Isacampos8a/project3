2ee1d981cdcc24b0682a1f63bd0a45accdba62bb
Isabel Campos <isacampos8a@gmail.com>
2025-02-08 13:54:33 -0600

    Final Commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,45 @@
 <script setup>
+import {ref} from 'vue'
+
+let countdownInterval;
+const time = ref(0);
+const timerDisplay = ref('Time Left: 0 min 0sec')
+
+//audio
+const alarm = new Audio('morning_flower.mp3')
+
+function startTimer() {
+    const targetTime = time.value * 60;
+    let currentTime = targetTime;
 
 
+
+    //clears the countdown
+    if (countdownInterval){
+        clearInterval(countdownInterval);
+    }
+
+    countdownInterval = setInterval(() => {
+        const minutesLeft = Math.floor(currentTime / 60);
+        const secondsLeft = currentTime % 60;
+        timerDisplay.value = `Time Left: ${minutesLeft} min ${secondsLeft} sec`
+
+        if (currentTime == 0) {
+            clearInterval(countdownInterval);
+            timerDisplay.value = "Time's Up! It has reached peak egg-cellence"
+            alarm.play();
+
+        }    
+        currentTime--; // decrements the time
+    }, 1000); //updates every second
+
+    time.value = ''
+    timerDisplay.value = ''
+}
+
+
+
+ 
 </script>
 
 <template>
@@ -14,9 +53,16 @@
     <section class="time-input">
       <input type="number" placeholder="Enter time in minutes" v-model="time">
 
+      <div class="start-button" >
+        <button @click="startTimer">Start</button>
+      </div>
+
     </section>
 
-    <section class="timer">
+    <section class="timer-display">
+      <div id="timerDisplay">
+        <h4>{{timerDisplay}}</h4>
+      </div>
 
     </section>
   </main>
