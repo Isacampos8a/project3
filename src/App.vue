e9fa2ac593750921ee36ca5b4051fe11eb665016
Isabel Campos <isacampos8a@gmail.com>
2025-02-08 12:00:54 -0600

    initial commit of all the files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <main class="app">
+    <section class="main-title">
+      <h2>Egg Timer</h2>
+      <h3>by Isabel Campos</h3>
+
+    </section>
+
+    <section class="time-input">
+      <input type="number" placeholder="Enter time in minutes" v-model="time">
+
+    </section>
+
+    <section class="timer">
+
+    </section>
+  </main>
+  
+</template>
+
+
